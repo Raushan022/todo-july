6ac44b45919571489b9037b00319ca8dd1960383
raushan <kraushan022@gmail.com>
2025-07-19 16:34:16 +0530

    added stated and onchange function with handleaddTodo function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    tag: "",
+  });
+
+  //handle input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  //handle submit
+  const handleAddTodo = () => {
+    console.log(formData);
+  };
+
   return (
     <div className="max-w-md mx-auto mt-16 p-8 rounded-2xl shadow-xl bg-white">
       <h1 className="text-2xl text-gray-800 font-bold mb-6 text-center">
@@ -14,8 +31,11 @@ const Home = () => {
         </label>
         <input
           type="text"
+          name="title"
           placeholder="Enter Title"
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          onChange={handleChange}
+          value={formData.title}
+          className=" text-gray-700 w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
       </div>
 
@@ -25,9 +45,12 @@ const Home = () => {
           Description
         </label>
         <textarea
+          name="description"
           placeholder="Enter Description"
           rows="3"
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
+          onChange={handleChange}
+          value={formData.description}
+          className=" text-gray-700 w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 resize-none"
         ></textarea>
       </div>
 
@@ -38,14 +61,17 @@ const Home = () => {
         </label>
         <input
           type="text"
+          name="tag"
           placeholder="Enter Tag (e.g., Work)"
-          className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          onChange={handleChange}
+          value={formData.tag}
+          className=" text-gray-700 w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
       </div>
 
       <button
         className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 rounded-xl transition duration-200"
-        onClick={() => console.log("hello")}
+        onClick={handleAddTodo}
       >
         ➕ Add Todo
       </button>
